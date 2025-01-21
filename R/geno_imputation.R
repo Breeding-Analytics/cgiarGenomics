88518a4a37175e9407ff96eb08d4d4841d6b34f6
serbier <serbier@users.noreply.github.com>
2025-01-21 14:06:34 -0500

    ADD imputation accuracy functions

--- R/geno_imputation.R
+++ R/geno_imputation.R
@@ -103,3 +103,73 @@ impute_gl <- function(gl, ploidity = 2, method = 'frequency'){
   
   return(list(gl = imp_gl, log = imp_dict))
 }
+
+
+#' Mask a fraction of genotype calls for measure the 
+#' accuracy of an imputation method
+#'
+#' @param gl A genlight instance
+#' @param fraction percentage of data to mask [0 - 1], default = 0.1
+#'
+#' @return A genlight instance where the genotype matrix have as NA
+#' the given fraction of missing data
+#' @export
+#'
+#' @examples
+set_fraction_na <- function(gl, fraction = 0.1) {
+    # Ensure fraction is between 0 and 1
+    if (!is.numeric(fraction) || fraction < 0 || fraction > 1) {
+      stop("fraction must be a number between 0 and 1")
+    }
+    m <- as.matrix(gl)
+    
+    # Get total number of elements
+    total_elems <- length(m)
+    
+    # Compute how many elements to replace
+    n_replace <- round(total_elems * fraction)
+    
+    # Randomly choose positions
+    positions <- sample(seq_len(total_elems), size = n_replace, replace = FALSE)
+    
+    # Replace chosen positions with NA
+    m[positions] <- NA
+    
+    masked_gl <- new("genlight", m, ploidy=max(gl@ploidy))
+    masked_gl <- recalc_metrics(masked_gl)
+    return(masked_gl)
+}
+
+
+#' Compare the genotype calls (gt) of a reference genlight object
+#' against an imputed gl. Measure the accuracy of the imputation
+#' process comparing the reference gt with imputed gt
+#'
+#' @param ref_gl Rreference genlight instance
+#' @param imp_gl Imputed genlight instance
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_accuracy <- function(ref_gl, imp_gl){
+  # Get the gt matix of the two gls
+  ref_m <- as.matrix(ref_gl)
+  imp_m <- as.matrix(imp_gl)
+  # Boolean matrix where the elements match/missmatch
+  comp_m <- ref_m == imp_m
+  # Boolean matrix where the element is NA
+  na_m <- is.na(ref_m)
+  target_cells <- which(na_m)
+  
+  # Convert to NA the element where in ref is NA (not comparable)
+  comp_m[target_cells] <- NA
+  # Sum by column the correctly imputed gt
+  matchs_sums <- colSums(comp_m, na.rm = T)
+  accuracy <- matchs_sums/colSums(!na_m)
+  
+  return(accuracy)
+}
+
+
+
